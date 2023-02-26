5b0c8785996008446fdf2e99308596ad1b47b01d
TalanetsLiza <yarmantovich1999@mail.ru>
2023-02-27 01:40:26 +0300

    add: complete task 7

---
+++ scripts/task7.js
@@ -0,0 +1,9 @@
+{
+    const s = 2 * 10 ** 6;
+    const p = 10; // percent
+    const years = 5;
+    const Pereplata = (s * p / 100) * years;
+
+    console.log("\nTask 7");
+    console.log(Pereplata);
+}

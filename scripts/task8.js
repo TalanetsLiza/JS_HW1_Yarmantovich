46d13589706e34cfadc03fb9d127087dd8c93d3f
TalanetsLiza <yarmantovich1999@mail.ru>
2023-02-27 18:02:33 +0300

    add: complete task 8

---
+++ scripts/task8.js
@@ -0,0 +1,13 @@
+{  
+  const a = 8;
+  const b = 3;
+
+  const x1 = (16 + 2 * b - a) / 2;
+  const x2 = (b * 15 - a) / (6 - b);
+  const x3 = 23780 / (3 + a + b);
+
+  console.log("\nTask 8");
+  console.log(x1);
+  console.log(x2);
+  console.log(x3);
+}
